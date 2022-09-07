ab59f7a642a61681efa2e22ef5a65ab3a1dfd498
terminaate <tntwnik@gmail.com>
2022-09-07 16:30:44 +0300

    Add suspense component to routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, lazy, Suspense } from 'react';
 import { Route, Routes, useLocation } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
 import { useAppDispatch, useAppSelector } from '@/store';
@@ -8,13 +8,11 @@ import { refresh } from '@/store/reducers/user/authAPI';
 import { getUser, getUserPosts } from '@/store/reducers/user/userAPI';
 import Header from '@/components/Header';
 
-const SettingsPage = React.lazy(() => import('@/pages/SettingsPage'));
-const UserPage = React.lazy(() => import('@/pages/UserPage'));
-const LoginPage = React.lazy(() => import('@/pages/AuthPages/LoginPage'));
-const RegisterPage = React.lazy(() => import('@/pages/AuthPages/RegisterPage'));
+const SettingsPage = lazy(() => import('@/pages/SettingsPage'));
+const UserPage = lazy(() => import('@/pages/UserPage'));
+const LoginPage = lazy(() => import('@/pages/AuthPages/LoginPage'));
+const RegisterPage = lazy(() => import('@/pages/AuthPages/RegisterPage'));
 
-// TODO
-// Пофиксить баг с переходом на страницу пользователя из любой не известной страницы
 
 const App = () => {
 	const location = useLocation();
@@ -42,21 +40,23 @@ const App = () => {
 		<>
 			<Header />
 			<AnimatePresence exitBeforeEnter>
-				<Routes location={location} key={location.pathname}>
-					<Route path={'/login'} element={<LoginPage />} />
-					<Route path={'/register'} element={<RegisterPage />} />
-					<Route path={'/users/:id'} element={
-						<AuthorizedRoute>
-							<UserPage />
-						</AuthorizedRoute>
-					} />
-					<Route path={'/settings'} element={
-						<AuthorizedRoute>
-							<SettingsPage />
-						</AuthorizedRoute>
-					} />
-					<Route path={'/*'} element={<span>404</span>} />
-				</Routes>
+				<Suspense fallback={<span>Loading</span>}>
+					<Routes location={location} key={location.pathname}>
+						<Route path={'/login'} element={<LoginPage />} />
+						<Route path={'/register'} element={<RegisterPage />} />
+						<Route path={'/users/:id'} element={
+							<AuthorizedRoute>
+								<UserPage />
+							</AuthorizedRoute>
+						} />
+						<Route path={'/settings'} element={
+							<AuthorizedRoute>
+								<SettingsPage />
+							</AuthorizedRoute>
+						} />
+						<Route path={'/*'} element={<span>404</span>} />
+					</Routes>
+				</Suspense>
 			</AnimatePresence>
 		</>
 	);
