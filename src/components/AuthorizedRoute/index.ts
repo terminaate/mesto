4b1f24d2b53f4311bb70feeb9f63e6cbe665900f
terminaate <tntwnik@gmail.com>
2022-10-20 21:02:45 +0300

    first version of hygen, and move AuthorizedRoute to components folder

--- src/components/AuthorizedRoute/index.ts
+++
@@ -1,3 +0,0 @@
-import AuthorizedRoute from './AuthorizedRoute';
-
-export default AuthorizedRoute;
--- src/pages/SettingsPage/SettingsAccountPage/SettingsAccountPage.tsx
+++ src/pages/SettingsPage/SettingsAccountPage/SettingsAccountPage.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import cl from "./SettingsAccountPage.module.scss";
 import { useAppSelector } from '@/store';
 import BasicSettingsPage from '@/pages/SettingsPage/BasicSettingsPage/BasicSettingsPage';
+import Input from '@/components/UI/Input';
 
 const SettingsAccountPage = () => {
   const {user} = useAppSelector(state => state.userSlice);
 
   return (
     <BasicSettingsPage className={cl.accountPage}>
-      <img src={user.avatar} alt='' />
+      <Input placeholder={"123"}/>
     </BasicSettingsPage>
   );
 };
